c2cd29b7b4c79c5411c5b6a9e3df3a4eb05be615
vandervaeren <melvdv@yahoo.fr>
2022-08-29 22:27:57 +0200

    version web ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  { 
+    apiKey: "AIzaSyDQBurum1z-3OFUzud6pIeMVPal-3uyigE",
+  authDomain: "dermoguide-77fb8.firebaseapp.com",
+  projectId: "dermoguide-77fb8",
+  storageBucket: "dermoguide-77fb8.appspot.com",
+  messagingSenderId: "557975490031",
+  appId: "1:557975490031:web:126f84e2a086465b007a1a",
+  measurementId: "G-32LLDMFQ5T"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  { 
+  apiKey: "AIzaSyDQBurum1z-3OFUzud6pIeMVPal-3uyigE",
+  authDomain: "dermoguide-77fb8.firebaseapp.com",
+  projectId: "dermoguide-77fb8",
+  storageBucket: "dermoguide-77fb8.appspot.com",
+  messagingSenderId: "557975490031",
+  appId: "1:557975490031:web:126f84e2a086465b007a1a",
+  measurementId: "G-32LLDMFQ5T"
+  }
 };
 
 /*
